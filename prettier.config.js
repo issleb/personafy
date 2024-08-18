e8fff8da1e824c625d9c55013abee622dfe0535d
Brian Issleb <brian@getvesta.io>
2024-08-18 15:08:34 -0500

    Fix lint error.

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 // https://prettier.io/docs/en/options.html
 /** @type {import('prettier').RequiredOptions} */
+// eslint-disable-next-line no-undef
 module.exports = {
   trailingComma: 'es5',
   bracketSpacing: true,
